28de836298dc93cc5c3ff9dbc4dafb179cafbbbf
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-08-30 11:12:34 +0800

    数据绑定

--- components/like/index.js
+++ components/like/index.js
@@ -1,4 +1,3 @@
-// components/like/index.js
 Component({
   /**
    * 组件的属性列表
@@ -8,16 +7,23 @@ Component({
   },
 
   /**
-   * 组件的初始数据
+   * 组件内部的数据定义在此
    */
   data: {
-
+    // 数据绑定
+    like:false,
+    count1:99,
+    count2:999,
+    likeSrc:"images/like.png",
+    unLikeSrc:'images/like@dis.png'
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onLike:function(event){
+      
+    }
   }
 })
