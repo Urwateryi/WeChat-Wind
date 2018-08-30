957859fb554c015d48c8eb3621dc20b39d427d91
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-08-30 15:22:59 +0800

    网络请求添加错误处理

--- util/http.js
+++ util/http.js
@@ -3,6 +3,12 @@ import {
   config
 } from '../config.js'
 
+const tips={
+  1:'抱歉！出现了一个错误',
+  1005:'appkey无效，请前往www.7yue.pro申请',
+  3000:'期刊不存在'
+}
+
 class HTTP {
   request(params) {
     if(!params.method){
@@ -19,14 +25,25 @@ class HTTP {
       success:(res)=>{
         let code=res.statusCode.toString()
         if(code.startsWith('2')){
-
-        }else{
-
+            params.success(res.data)
+        }else{//异常，
+          this._show_error(res.data.error_code)
         }
       },
-      fail:(err)=>{
-
+      fail:(err)=>{//服务器调用失败
+        this._show_error(1)
       }
+    })
+  }
+
+  _show_error(error_code){
+    if(!error_code){
+      error_code=1
+    }
+    wx.showToast({
+      title: tips[error_code],
+      icon:'none',
+      duration:2000
     })
   }
 }
