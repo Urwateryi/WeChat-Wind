2b8c408cd698a634c6fcbb1b2e12d610de65f48b
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-08-30 16:44:26 +0800

    首页中间部分页面的component

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -5,12 +5,13 @@ let classic = new ClassicModel();
 Page({
 
   data: {
-
+    classicData:null
   },
   onLoad: function (options) {
     classic.getLastest((res)=>{
         console.log(res)
         this.setData({
+          // classicData是要传递到wxml中去，被wxml使用的变量，最好在data中初始化一下
           classicData:res
         })
     })
