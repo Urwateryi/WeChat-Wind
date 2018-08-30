0ff2cd0d09b5a8baeb1979e3b0a5d80ef99188c2
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-08-30 14:30:51 +0800

    网络请求

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -1,7 +1,7 @@
 Page({
 
   data: {
-    test:1
+
   },
   onLoad: function (options) {
     wx.request({
@@ -9,59 +9,9 @@ Page({
       header:{
         appkey:"K5H3SiFRY9qLzs0n"
       },
-      //使用箭头函数
       success:(res)=>{
-        console.log(this.data.test)
+        
       }
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
 })
