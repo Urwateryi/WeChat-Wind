3d3ecdfce2715905afbccf6072d31beaa0ef29db
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-08-31 16:29:07 +0800

    为首页的上一页下一页加入缓存

--- models/classic.js
+++ models/classic.js
@@ -9,17 +9,32 @@ class ClassicModel extends HTTP {
       success: (res) => {
         sCallback(res)
         this._setLatestIndex(res.index)
+
+        //写入缓存
+        let key=this._getKey(res.index)
+        wx.setStorageSync(key, res)
       }
     })
   }
 
   getClassic(index,nextOrPrevious, sCallback) {
-    this.request({
-      url: 'classic/' + index + '/' + nextOrPrevious,
-      success: (res) => {
-        sCallback(res)
-      }
-    })
+    let key=nextOrPrevious=='next'?this._getKey(index+1):this._getKey(index-1)
+    //从缓存中找
+    let classicCache=wx.getStorageSync(key)
+    //如果没找到
+    if (!classicCache){
+      this.request({
+        url: 'classic/' + index + '/' + nextOrPrevious,
+        success: (res) => {
+          //写入缓存
+          wx.setStorageSync(this._getKey(res.index), res)
+          sCallback(res)
+        }
+      })
+    }else{
+      //如果找到了，直接就用缓存的
+      sCallback(classicCache)
+    }
   }
 
   isFirst(index) {
@@ -40,6 +55,11 @@ class ClassicModel extends HTTP {
   //获取最新一期的index
   _getLatestIndex() {
     return wx.getStorageSync('latest')
+  }
+
+  _getKey(index){
+    let key='classic-'+index
+    return key
   }
 }
 
