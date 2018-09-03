36557b062b74e1bdf6f23ed1b9dc2a249a0153d4
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-09-03 13:36:38 +0800

    音乐后台播放

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,7 +1,9 @@
-import { classicBehavior } from '../classic-behavior.js'
+import {
+  classicBehavior
+} from '../classic-behavior.js'
 
 //音乐管理对象
-const mMgr=wx.getBackgroundAudioManager()
+const mMgr = wx.getBackgroundAudioManager()
 
 Component({
 
@@ -12,32 +14,53 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    src:String
+    src: String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    playing:false,
-    pauseSrc:'images/player@pause.png',
-    playSrc:'images/player@play.png',
+    playing: false,
+    pauseSrc: 'images/player@pause.png',
+    playSrc: 'images/player@play.png',
+  },
+
+  attached() {
+    this._recoverStatus()
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    onPlay(event){
+    onPlay(event) {
       //修改播放按钮的图片及状态
       this.setData({
         playing: !this.data.playing
       })
       //给一个播放音乐的src
-      if(!this.data.playing){
+      if (!this.data.playing) {
         mMgr.pause()
-      }else{
+      } else {
         mMgr.src = this.properties.src
+      }
+    },
+
+    //每次切换都检测一遍，如果当前后台没有音乐播放，则显示为暂停状态
+    _recoverStatus() {
+      if (mMgr.paused) {
+        this.setData({
+          playing: false
+        })
+        return
+      }
+
+      //如果当前正在播放的背景音乐和该组件的背景音乐地址一致，则就显示为播放状态
+      if (mMgr.src == this.properties.src) {
+        this.setData({
+          playing: true
+        })
       }
     }
   }
