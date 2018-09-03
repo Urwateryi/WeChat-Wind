a59608d08a17d763b2265367b82458981ccd480b
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-09-03 11:45:00 +0800

    音乐播放

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,4 +1,8 @@
 import { classicBehavior } from '../classic-behavior.js'
+
+//音乐管理对象
+const mMgr=wx.getBackgroundAudioManager()
+
 Component({
 
   behaviors: [
@@ -15,6 +19,7 @@ Component({
    * 组件的初始数据
    */
   data: {
+    playing:false,
     pauseSrc:'images/player@pause.png',
     playSrc:'images/player@play.png',
   },
@@ -23,6 +28,17 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onPlay(event){
+      //修改播放按钮的图片及状态
+      this.setData({
+        playing: !this.data.playing
+      })
+      //给一个播放音乐的src
+      if(!this.data.playing){
+        mMgr.pause()
+      }else{
+        mMgr.src = this.properties.src
+      }
+    }
   }
 })
