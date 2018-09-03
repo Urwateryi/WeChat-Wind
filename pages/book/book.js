6e63a3ab0f7b06f21791e09baa5222279b46bd47
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-09-03 14:43:43 +0800

    promise的基本用法

--- pages/book/book.js
+++ pages/book/book.js
@@ -5,62 +5,80 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+    const promise = new Promise((resolve, reject) => {
+      wx.getSystemInfo({
+        success(res) {
+          //信息获取成功后，通过resolve将状态修改为resolve
+          resolve(res)
+        },
+        fail(error) {
+          //信息获取失败后，通过resolve将状态修改为reject
+          reject(res)
+        }
+      })
+    })
+
+    //then有两个参数，一个是成功，一个是失败，分别对应上面的resolve和reject
+    promise.then((res) => {
+      console.log(res)
+    }, (error) => {
+      console.log(error)
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
