1b2ca28e4b9b714e2e444513698439c843c56b23
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-09-03 17:38:01 +0800

    book-detail

--- components/book/index.js
+++ components/book/index.js
@@ -1,4 +1,3 @@
-// components/book/index.js
 Component({
   /**
    * 组件的属性列表
@@ -18,6 +17,11 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onTap(event){
+      const bid=this.properties.book.id
+      wx.navigateTo({
+        url: `/pages/book-detail/book-detail?bid=${bid}`,
+      })
+    }
   }
 })
---
+++ pages/book-detail/book-detail.js
@@ -0,0 +1,11 @@
+Page({
+
+  data: {
+    
+  },
+
+  onLoad: function (options) {
+    const bid=options.bid
+    console.log(bid)
+  },
+})
