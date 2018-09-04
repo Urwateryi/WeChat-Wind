e334390b23dd1138ad99439258f6394415f1934b
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-09-04 10:50:01 +0800

    书籍详情页数据获取

--- models/book.js
+++ models/book.js
@@ -3,6 +3,7 @@ import {
 } from '../util/http-p.js'
 
 class BookModel extends HTTP {
+  //获取热门书籍列表
   getHotList() {
     return this.request({
       url:'book/hot_list'
@@ -12,6 +13,27 @@ class BookModel extends HTTP {
   getMyBookCount(){
     return this.request({
       url: '/book/favor/count'
+    })
+  }
+
+  //获取书籍的详情
+  getDetail(bid){
+    return this.request({
+      url:`book/${bid}/detail`
+    })
+  }
+
+  //获取当前图书的点赞状态
+  getLikeStatus(bid){
+    return this.request({
+      url:`/book/${bid}/favor`
+    })
+  }
+
+  //获取当前图书的短评信息
+  getComments(bid){
+    return this.request({
+      url:`book/${bid}/short_comment`
     })
   }
 }
--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -1,11 +1,42 @@
+import {
+  BookModel
+} from '../../models/book.js'
+
+const bookModel = new BookModel()
+
 Page({
 
   data: {
-    
+    comments:[],
+    book:null,
+    listStatus:false,
+    likeCount:0
   },
 
-  onLoad: function (options) {
-    const bid=options.bid
-    console.log(bid)
+  onLoad: function(options) {
+    const bid = options.bid
+
+    const detail = bookModel.getDetail(bid)
+    const comments = bookModel.getComments(bid)
+    const likeStatus = bookModel.getLikeStatus(bid)
+
+    detail.then(res=>{
+      this.setData({
+        book: res
+      })
+    })
+
+    comments.then(res=>{
+      this.setData({
+        comments:res
+      })
+    })
+
+    likeStatus.then(res => {
+      this.setData({
+        listStatus: res.like_status,
+        likeCount:res.fav_nums
+      })
+    })
   },
 })
