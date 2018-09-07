ded85c24841a153b8b71f73d762352517460137d
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-09-07 15:50:26 +0800

    发布评论

--- models/book.js
+++ models/book.js
@@ -6,34 +6,46 @@ class BookModel extends HTTP {
   //获取热门书籍列表
   getHotList() {
     return this.request({
-      url:'book/hot_list'
+      url: 'book/hot_list'
     })
   }
 
-  getMyBookCount(){
+  getMyBookCount() {
     return this.request({
       url: '/book/favor/count'
     })
   }
 
   //获取书籍的详情
-  getDetail(bid){
+  getDetail(bid) {
     return this.request({
-      url:`book/${bid}/detail`
+      url: `book/${bid}/detail`
     })
   }
 
   //获取当前图书的点赞状态
-  getLikeStatus(bid){
+  getLikeStatus(bid) {
     return this.request({
-      url:`/book/${bid}/favor`
+      url: `/book/${bid}/favor`
     })
   }
 
   //获取当前图书的短评信息
-  getComments(bid){
+  getComments(bid) {
     return this.request({
-      url:`book/${bid}/short_comment`
+      url: `book/${bid}/short_comment`
+    })
+  }
+
+  // 发送评论
+  postComment(bid, comment) {
+    return this.request({
+      url: 'book/add/short_comment',
+      method: 'POST',
+      data: {
+        book_id: bid,
+        content: comment
+      }
     })
   }
 }
--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -1,9 +1,9 @@
 import {
   BookModel
 } from '../../models/book.js'
-import{
+import {
   LikeModel
-}from '../../models/like.js'
+} from '../../models/like.js'
 
 const bookModel = new BookModel()
 const likeModel = new LikeModel()
@@ -11,11 +11,11 @@ const likeModel = new LikeModel()
 Page({
 
   data: {
-    comments:[],
-    book:null,
-    listStatus:false,
-    likeCount:0,
-    posting:false//用户是否打开了输入框
+    comments: [],
+    book: null,
+    listStatus: false,
+    likeCount: 0,
+    posting: false //用户是否打开了输入框
   },
 
   onLoad: function(options) {
@@ -25,13 +25,13 @@ Page({
     const comments = bookModel.getComments(bid)
     const likeStatus = bookModel.getLikeStatus(bid)
 
-    detail.then(res=>{
+    detail.then(res => {
       this.setData({
         book: res
       })
     })
 
-    comments.then(res=>{
+    comments.then(res => {
       this.setData({
         comments: res.comments
       })
@@ -40,25 +40,60 @@ Page({
     likeStatus.then(res => {
       this.setData({
         listStatus: res.like_status,
-        likeCount:res.fav_nums
+        likeCount: res.fav_nums
       })
     })
   },
 
-  onLike(event){
+  onLike(event) {
     const list_or_cancel = event.detail.behavior
     likeModel.like(list_or_cancel, this.data.book.id, 400);
   },
 
-  onFakePost(event){
+  onFakePost(event) {
     this.setData({
-      posting:true
+      posting: true
     })
   },
 
-  onCancel(event){
+  onCancel(event) {
     this.setData({
       posting: false
     })
-  }
+  },
+
+  onPost(event) {
+    const comment = event.detail.text || event.detail.value //前者为点击tag获取到的内容，后者为在input组件内部获取到的内容
+
+    if (!comment) {
+      return
+    }
+
+    if (comment.length > 12) {
+      wx.showToast({
+        title: '短评最多12个字',
+        icon: 'none'
+      })
+      return
+    }
+
+    bookModel.postComment(this.data.book.id, comment)
+      .then(res => {
+        wx.showToast({
+          title: '+ 1',
+          icon: 'none'
+        })
+
+        //unshift() 方法可向数组的开头添加一个或更多元素，并返回新的长度
+        this.data.comments.unshift({
+          content: comment,
+          nums: 1
+        })
+
+        this.setData({
+          comments: this.data.comments,
+          posting: false
+        })
+      })
+  },
 })
